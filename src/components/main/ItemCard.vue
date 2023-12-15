677e98f236bcd34d46cd34df8f2479eee9052e12
MatteoSanson <matteo.sanson@gmail.com>
2023-12-15 14:27:49 +0100

    feat: milestone2

--- src/components/main/ItemCard.vue
+++ src/components/main/ItemCard.vue
@@ -18,11 +18,45 @@ export default {
             <li>
                 <h3>Titolo: {{ titolo }}</h3>
                 <h4>Titolo originale: {{ original }}</h4>
-                <p>Lingua: {{ lingua }}</p>
+                <div class="language" v-if="lingua === 'it'">
+                    <p>
+                        Lingua: {{ lingua }}
+                    </p>
+                    <div class="img-container">
+                        <img src="../../assets/pics/istockphoto-166053218-612x612.jpg" alt="">
+                    </div>
+                </div>
+                <div class="language" v-else-if="lingua === 'en'">
+                    <p>
+                        Lingua: {{ lingua }}
+                    </p>
+                    <div class="img-container">
+                        <img src="../../assets/pics/Flag_of_Great_Britain_(English_version).png" alt="">
+                    </div>
+                </div>
+                <p v-else>Lingua: {{ lingua }}</p>
+
                 <p>Voto: {{ voto }}</p>
             </li>
         </ul>
     </div>
 </template>
 
-<style></style>
+<style>
+.language {
+    display: flex;
+    width: 100px;
+    align-items: center;
+    justify-content: space-between;
+}
+
+.img-container {
+    width: 25px;
+    height: 20px;
+    object-fit: contain;
+}
+
+.img-container img {
+    width: 100%;
+}
+</style>
