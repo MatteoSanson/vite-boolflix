7bd8b6de0c9915fb7faaa0c588cf475330179d5e
MatteoSanson <matteo.sanson@gmail.com>
2023-12-18 21:22:25 +0100

    feat: dimensioni footer

--- src/components/footer/AppFooter.vue
+++ src/components/footer/AppFooter.vue
@@ -10,4 +10,11 @@ export default {
     </footer>
 </template>
 
-<style></style>
+<style scoped lang="scss">
+@use '../../assets/scss/partials/variables.scss' as *;
+
+footer {
+    height: 100px;
+    background-color: $primary-bg-color;
+}
+</style>
