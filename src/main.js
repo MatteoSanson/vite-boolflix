bacc0a77fe07d5310c8714753b42b9e38479c4a4
MatteoSanson <matteo.sanson@gmail.com>
2023-12-18 14:30:38 +0100

    script: install sass

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue';
-import './style.css';
+/* file stile scss*/
+import './style.scss';
+
 import App from './App.vue';
 /* import the fontawesome core */
 import { library } from '@fortawesome/fontawesome-svg-core';
